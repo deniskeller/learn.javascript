fa2b5974cb2c4b1734b913e8431fdf9a02c9d552
deniskeller <dentean@mail.ru>
2024-11-14 22:20:59 +0400

    logical-operators

---
+++ js/logical-operators.js
@@ -0,0 +1,33 @@
+// let age = prompt('Введите число:', '');
+// console.log('age: ', age);
+
+// if (+age >= 14 && +age <= 90) {
+//   console.log('Значение "age" в текущем диапазоне');
+// } else {
+//   console.log('Значение "age" НЕ в текущем диапазоне');
+// }
+
+// if (+age <= 14 && +age >= 90) {
+//   console.log('Значение "age" ВНЕ текущего диапазона');
+// } else {
+//   console.log('Значение "age" в текущем диапазоне');
+// }
+
+// let login = prompt('Введите логин:', '');
+// let password;
+
+// if (login === 'админ') {
+//   password = prompt('Введите пароль:', '');
+
+//   if (password === 'главный') {
+//     alert('Здравствуйте!');
+//   } else if (password === '' || password === null) {
+//     alert('Отменено');
+//   } else {
+//     alert('Неверный пароль');
+//   }
+// } else if (login === '' || login === null) {
+//   alert('Отменено');
+// } else {
+//   alert('Я вас не знаю');
+// }
