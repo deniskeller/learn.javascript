2c28c7449510e98c1367a232a73661f7687b05dd
deniskeller <dentean@mail.ru>
2024-11-14 14:56:23 +0400

    git init

---
+++ js/ifelse.js
@@ -0,0 +1,17 @@
+// let value = prompt('Какое «официальное» название JavaScript?', '');
+
+// if (value == 'ECMAScript') {
+//   alert('Верно');
+// } else {
+//   alert('Не знаете? ECMAScript!');
+// }
+
+// let value = prompt('Введите число:', '');
+
+// if (value > 0) {
+//   alert('1');
+// } else if (value < 0) {
+//   alert('-1');
+// } else if (value == 0) {
+//   alert('0');
+// }
