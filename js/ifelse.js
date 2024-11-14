2badf6af0cca00a900537d8f6315df676ffbcc6b
deniskeller <dentean@mail.ru>
2024-11-14 15:11:34 +0400

    ifelse

--- js/ifelse.js
+++ js/ifelse.js
@@ -15,3 +15,37 @@
 // } else if (value == 0) {
 //   alert('0');
 // }
+
+// let result;
+// let a = prompt('Введите число a:', '');
+// let b = prompt('Введите число b:', '');
+// // if (a + b < 4) {
+// //   result = 'Мало';
+// // } else {
+// //   result = 'Много';
+// // }
+
+// result = +a + +b < 4 ? 'Мало' : 'Много';
+// console.log('result: ', result);
+
+// if (login == 'Сотрудник') {
+//   message = 'Привет';
+// } else if (login == 'Директор') {
+//   message = 'Здравствуйте';
+// } else if (login == '') {
+//   message = 'Нет логина';
+// } else {
+//   message = '';
+// }
+
+// let login = prompt('Кто вы?', '');
+// let message =
+//   login == 'Сотрудник'
+//     ? 'Привет'
+//     : login == 'Директор'
+//     ? 'Здравствуйте'
+//     : login == ''
+//     ? 'Нет логина'
+//     : '';
+
+// console.log('message: ', message);
