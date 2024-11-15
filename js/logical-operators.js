7f37a5eab18568e8a4662b4e94a1324aea7041e1
deniskeller <dentean@mail.ru>
2024-11-15 17:39:05 +0400

    while-for

--- js/logical-operators.js
+++ js/logical-operators.js
@@ -31,3 +31,17 @@
 // } else {
 //   alert('Я вас не знаю');
 // }
+
+// let num1 = 10,
+//   num2 = 20,
+//   result;
+
+// if (result === null || result === undefined) {
+//   if (num1 !== null && num1 !== undefined) {
+//     result = num1;
+//   } else {
+//     result = num2;
+//   }
+// }
+
+// result ??= num1 ?? num2;
---
+++ js/while-for.js
@@ -0,0 +1,42 @@
+// for (let i = 0; i < 3; i++) {
+//   console.log(`number ${i}!`);
+// }
+
+// let i = 0;
+// while (i < 3) {
+// 	console.log(`number ${i}!`);
+//   i++;
+// }
+
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(`number ${i}!`);
+//   }
+// }
+
+// let value = 0;
+
+// while (value <= 100) {
+//   value = prompt(`Введите число больше 100`, '');
+//   console.log(value);
+//   if (value == null || value == undefined) break;
+// }
+
+// let n = 10;
+
+// function isPrime(num) {
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) return false;
+//   }
+//   return true;
+// }
+
+// const computedPrimes = (n) => {
+//   for (let i = 2; i <= n; i++) {
+//     if (isPrime(i)) {
+//       console.log(i);
+//     }
+//   }
+// };
+
+// computedPrimes(10);
