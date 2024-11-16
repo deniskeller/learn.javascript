09d888663038cdb3cae238b31be52ce00837772e
deniskeller <dentean@mail.ru>
2024-11-17 01:07:13 +0400

    object 4.1

---
+++ js/object.js
@@ -0,0 +1,54 @@
+// const user = {};
+// user.name = 'John';
+// user.surname = 'Smith';
+// console.log('user: ', user);
+// user.name = 'Pete';
+// console.log('user: ', user);
+// delete user.name;
+// console.log('user: ', user);
+
+// let schedule = {};
+// function isEmpty(obj) {
+//   for (let key in obj) {
+//     if (key) return false;
+//   }
+//   return true;
+// }
+
+// console.log(isEmpty(schedule));
+// schedule['8:30'] = 'get up';
+// console.log(isEmpty(schedule));
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+// let sum = 0;
+
+// function calcSalaries(obj) {
+//   for (let key in obj) {
+//     sum += obj[key];
+//   }
+// }
+
+// calcSalaries(salaries);
+// console.log('sum: ', sum);
+
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: 'My menu',
+// };
+// console.log('menu: ', menu);
+
+// function multiplyNumeric(obj) {
+//   for (let key in obj) {
+//     if (typeof obj[key] === 'number') {
+//       obj[key] *= 2;
+//     }
+//   }
+// }
+
+// multiplyNumeric(menu);
+// console.log('menu: ', menu);
