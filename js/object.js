e2da45ef19c69d02ed595883c37222d7bffece1c
deniskeller <dentean@mail.ru>
2024-11-18 00:32:03 +0400

    object 4.4, 4.5

--- js/object.js
+++ js/object.js
@@ -52,3 +52,69 @@
 
 // multiplyNumeric(menu);
 // console.log('menu: ', menu);
+
+// let calculator = {
+//   read() {
+//     this.a = +prompt('Введите первое значение', '');
+//     this.b = +prompt('Введите второе значение', '');
+//   },
+//   sum() {
+//     return this.a + this.b;
+//   },
+
+//   mul() {
+//     return this.a * this.b;
+//   },
+// };
+
+// console.log('calculator: ', calculator);
+
+// calculator.read();
+// console.log('calculator: ', calculator);
+
+// console.log('calculator.sum(): ', calculator.sum());
+// console.log('calculator.mul(): ', calculator.mul());
+
+// console.log('calculator: ', calculator);
+
+// function Calculator() {
+//   this.read = function () {
+//     this.a = +prompt('Введите первое значение', '');
+//     this.b = +prompt('Введите второе значение', '');
+//   };
+//   this.sum = function () {
+//     return this.a + this.b;
+//   };
+
+//   this.mul = function () {
+//     return this.a * this.b;
+//   };
+//   console.log('this: ', this);
+// }
+
+// let calculator = new Calculator();
+
+// console.log('calculator: ', calculator);
+
+// calculator.read();
+// console.log('calculator: ', calculator);
+
+// console.log('Sum=' + calculator.sum());
+// console.log('Mul=' + calculator.mul());
+
+// console.log('calculator: ', calculator);
+
+// function Accumulator(startingValue) {
+//   this.value = startingValue;
+//   this.read = function () {
+//     let newValue = +prompt('Введите значение', 0);
+//     this.value += newValue;
+//     return this;
+//   };
+// }
+
+// let accumulator = new Accumulator(1);
+// accumulator.read(); // прибавляет введённое пользователем значение к текущему значению
+// accumulator.read(); // прибавляет введённое пользователем значение к текущему значению
+
+// console.log('accumulator value', accumulator.value);
