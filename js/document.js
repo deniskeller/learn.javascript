ffcef79d2486dd9a54cfa1ba63026c7f14682a9d
deniskeller <dentean@mail.ru>
2024-12-10 20:33:26 +0400

    styles-and-classes

--- js/document.js
+++ js/document.js
@@ -181,26 +181,26 @@
 // --------------------------------------------------------- Стили и классы -------------------------------------------------------------------
 
 // Создать уведомление
-function showNotification({ top = 0, right = 0, className, html }) {
-  const notification = document.createElement('div');
-  notification.classList.add('notification', className);
-  notification.style.top = top + "px";
-  notification.style.right = right + "px";
-  notification.innerHTML = html;
-  const app = document.querySelector('#app')
-	app.append(notification);
-	setTimeout(() => {
-		notification.remove()
-	}, 1500);
-}
+// function showNotification({ top = 0, right = 0, className, html }) {
+//   const notification = document.createElement('div');
+//   notification.classList.add('notification', className);
+//   notification.style.top = top + "px";
+//   notification.style.right = right + "px";
+//   notification.innerHTML = html;
+//   const app = document.querySelector('#app')
+// 	app.append(notification);
+// 	setTimeout(() => {
+// 		notification.remove()
+// 	}, 1500);
+// }
 
-// test it
-let i = 1;
-setInterval(() => {
-  showNotification({
-    top: 10,
-    right: 10,
-    html: 'Hello ' + i++,
-    className: 'welcome',
-  });
-}, 2000);
+// // test it
+// let i = 1;
+// setInterval(() => {
+//   showNotification({
+//     top: 10,
+//     right: 10,
+//     html: 'Hello ' + i++,
+//     className: 'welcome',
+//   });
+// }, 2000);
