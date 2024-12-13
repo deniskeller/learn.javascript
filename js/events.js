f5619bdf7f7d5898d675dbf18de51685b5cf8c0b
deniskeller <dentean@mail.ru>
2024-12-13 15:16:29 +0400

    events 2

--- js/events.js
+++ js/events.js
@@ -14,33 +14,35 @@
 // });
 
 // ------ Передвиньте мяч по полю
-const ball = document.querySelector('#ball');
-const field = document.querySelector('#field');
-const fieldRect = field.getBoundingClientRect();
-console.log('fieldRect: ', fieldRect);
-const paddingTopBottom = (field.offsetWidth - field.clientWidth) / 2;
-const paddingLeftRight = (field.offsetHeight - field.clientHeight) / 2;
+// const ball = document.querySelector('#ball');
+// const field = document.querySelector('#field');
 
-field.addEventListener('click', function (event) {
-  console.log('Координаты: ' + event.clientX + ':' + event.clientY);
+// field.addEventListener('click', function (event) {
+//   console.log('Координаты: ' + event.clientX + ':' + event.clientY);
 
-  let left =
-    event.clientX - fieldRect.left - paddingLeftRight - ball.offsetWidth / 2;
+//   const fieldRect = field.getBoundingClientRect();
+//   console.log('fieldRect: ', fieldRect);
+//   const paddingTopBottom = (field.offsetWidth - field.clientWidth) / 2;
+//   const paddingLeftRight = (field.offsetHeight - field.clientHeight) / 2;
 
-  let top =
-    event.clientY - fieldRect.top - paddingTopBottom - ball.offsetHeight / 2;
+//   let top =
+//     event.clientY - fieldRect.top - paddingTopBottom - ball.offsetHeight / 2;
+//   let left =
+//     event.clientX - fieldRect.left - paddingLeftRight - ball.offsetWidth / 2;
 
-  console.log('left: ', left);
-  console.log('top: ', top);
-  console.log(field.clientWidth);
+//   console.log('left: ', left);
+//   console.log('top: ', top);
+//   console.log(field.clientWidth);
 
-  if (top < 0) top = 0;
-  if (left < 0) left = 0;
-  if (left + ball.offsetWidth > field.clientWidth)
-    left = field.clientWidth - ball.offsetWidth;
-  if (top + ball.offsetHeight > field.clientHeight)
-    top = field.clientHeight - ball.offsetHeight;
+//   if (top < 0) top = 0;
+//   if (left < 0) left = 0;
+//   if (left + ball.offsetWidth > field.clientWidth)
+//     left = field.clientWidth - ball.offsetWidth;
+//   if (top + ball.offsetHeight > field.clientHeight)
+//     top = field.clientHeight - ball.offsetHeight;
 
-  ball.style.left = left + 'px';
-  ball.style.top = top + 'px';
-});
+//   ball.style.left = left + 'px';
+//   ball.style.top = top + 'px';
+// });
+
+// ------ Создать раскрывающееся меню
