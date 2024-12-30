9ba5fa3c6903e1a81740790fba4149dccbb80363
deniskeller <dentean@mail.ru>
2024-12-30 23:44:17 +0400

    Form Properties and Methods task 1

--- js/forms-controls.js
+++ js/forms-controls.js
@@ -1,13 +1,20 @@
 // -------------------------------------------------------------------- Формы, элементы управления --------------------------------------------------------------------------
 
 // ------ Свойства и методы формы
-const genres = document.querySelector('#genres');
-genres.addEventListener('change', () => console.log('genres: ', genres.value));
-console.log('selected genres value: ', genres.options[genres.selectedIndex].value);
-console.log('selected genres text: ', genres.options[genres.selectedIndex].text);
-
-const newOption = new Option('Классика', 'classic');
-console.log('newOption: ', newOption);
-genres.append(newOption);
-newOption.selected = true;
-console.log('genres: ', genres.value);
+{
+  /* <select id="genres">
+  <option value="rock">Рок</option>
+  <option value="blues" selected>
+    Блюз
+  </option>
+</select>; */
+}
+// const genres = document.querySelector('#genres');
+// genres.addEventListener('change', () => console.log('genres: ', genres.value));
+// console.log('selected genres value: ', genres.options[genres.selectedIndex].value);
+// console.log('selected genres text: ', genres.options[genres.selectedIndex].text);
+// const newOption = new Option('Классика', 'classic');
+// console.log('newOption: ', newOption);
+// genres.append(newOption);
+// newOption.selected = true;
+// console.log('genres: ', genres.value);
