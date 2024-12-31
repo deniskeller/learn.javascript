091813d6f65b590079c3b46a6f740e9a7fabd9f7
deniskeller <dentean@mail.ru>
2024-12-31 19:30:14 +0400

    focus-blur tasks 1

--- js/forms-controls.js
+++ js/forms-controls.js
@@ -18,3 +18,52 @@
 // genres.append(newOption);
 // newOption.selected = true;
 // console.log('genres: ', genres.value);
+
+// --------------------------------------------------------------------------- Фокусировка: focus/blur --------------------------------------------------------------------
+
+// ------ Редактируемый div
+// .view,
+// .edit {
+// 	height: 150px;
+// 	width: 400px;
+// 	font-family: sans-serif;
+// 	font-size: 14px;
+// 	display: block;
+// }
+// .view {
+// 	/* padding + border = 3px */
+// 	padding: 2px;
+// 	border: 1px solid black;
+// }
+// .edit {
+// 	/* замена внутреннего отступа рамкой (по-прежнему 3px, и содержимое не смещается) */
+// 	border: 3px solid blue;
+// 	padding: 0px;
+// }
+// .edit:focus {
+// 	/* удаление обводки фокуса в Safari */
+// 	outline: none;
+// }
+// <ul>
+// 	<li>Кликните на div ниже, чтобы отредактировать текст.</li>
+// 	<li>Нажатие на Enter или потеря фокуса с ввода сохранят содержимое.</li>
+// </ul>
+// HTML-код разрешён.
+// <div id="view" class="view">Текст</div>
+// const view = document.querySelector('#view');
+// const textarea = document.createElement('textarea');
+// view.addEventListener('click', () => {
+//   textarea.classList.add('edit');
+//   textarea.value = view.textContent;
+//   view.replaceWith(textarea);
+//   textarea.focus();
+// });
+// textarea.addEventListener('keydown', (e) => {
+//   if (e.key == 'Enter') {
+//     textarea.blur();
+//   }
+// });
+// textarea.addEventListener('blur', () => {
+//   view.innerHTML = textarea.value;
+//   textarea.replaceWith(view);
+// });
